ec287ddfb65593fb8dc7b21580b4983b4ac6fc0d
SreejelKV <sreejel@keyvalue.systems>
2023-01-30 19:57:28 +0530

    Initial setup and Firebase google auth

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+  },
+};
---
+++ src/Firebase.js
@@ -0,0 +1,31 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD1pE3k0geZztodyNDyJfNP8MaJl_djXrU",
+    authDomain: "stockapp-55e10.firebaseapp.com",
+    projectId: "stockapp-55e10",
+    storageBucket: "stockapp-55e10.appspot.com",
+    messagingSenderId: "603638560564",
+    appId: "1:603638560564:web:fbc7f39e34a8acd775d761",
+    measurementId: "G-PBPQEQXJYL"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
+const provider = new GoogleAuthProvider();
+
+export const signInwithGoogle = () => {
+    signInWithPopup(auth, provider)
+        .then((result) => {
+            console.log(result);
+            sessionStorage.setItem("userAuthenticated",true);
+            sessionStorage.setItem("userName",result.user.displayName);
+            sessionStorage.setItem("userEmail",result.user.email);
+            sessionStorage.setItem("photo",result.user.photoURL);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+};
