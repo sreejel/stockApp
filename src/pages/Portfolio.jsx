badb463ebca5c4aeec93d602c49d91ecedc589bd
sreejel <sreejelanil1998@gmail.com>
2023-02-01 02:23:07 +0530

    minor changes

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 
-import PortfolioCard from '@/components/PortfolioCard';
+import PortfolioCard from '../components/PortfolioCard';
 
 function Portfolio() {
   const router = useRouter();
